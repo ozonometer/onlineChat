c4ff7c00caff20bd2b82a42bc175df8b7db6c79e
gusta <gjaco2@uis.edu>
2022-10-30 09:20:57 -0500

    message components routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent} from "./login/login.component";
 import { HomeComponent} from "./home/home.component";
 import {RegisterComponent} from "./register/register.component";
 import {ProfileComponent} from "./profile/profile.component";
+import {MessageHomeComponent} from "./message-home/message-home.component";
 
 const routes: Routes = [
   {path:"", component:LoginComponent},
   {path:"login", component:LoginComponent},
   {path:"home", component:HomeComponent},
   {path:"register", component:RegisterComponent},
-  {path:"profile", component:ProfileComponent}
+  {path:"profile", component:ProfileComponent},
+  {path:"message-home", component:MessageHomeComponent}
 ];
 
 @NgModule({
